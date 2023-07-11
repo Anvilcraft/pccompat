0a1216f888b8eb2baa8e75305d3d2b3037c7593b
Timo Ley <timo.ley@tilera.dev>
2023-07-11 12:43:18 +0200

    feat: hbm temp textures

--- src/main/java/net/anvilcraft/pccompat/blocks/BlockPowerConverterHBM.java
+++ src/main/java/net/anvilcraft/pccompat/blocks/BlockPowerConverterHBM.java
@@ -4,6 +4,7 @@ import covers1624.powerconverters.block.BlockPowerConverter;
 import covers1624.powerconverters.gui.PCCreativeTab;
 import net.anvilcraft.pccompat.tiles.TileEntityHBMConsumer;
 import net.anvilcraft.pccompat.tiles.TileEntityHBMProducer;
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 
@@ -14,7 +15,17 @@ public class BlockPowerConverterHBM extends BlockPowerConverter {
     }
 
     @Override
+    public void registerBlockIcons(IIconRegister reg) {
+        for (int i = 0; i < 4; i++) {
+            String state = i % 2 == 0 ? "off" : "on";
+            String type = i < 2 ? "consumer" : "producer";
+
+            this._icons[i] = reg.registerIcon("pccompat:ue_mv_" + type + "_" + state); //TODO: create unique textures for HBM
+        }
+    }
+
+    @Override
     public TileEntity createNewTileEntity(World world, int meta) {
-        return meta == 0 ? new TileEntityHBMProducer() : new TileEntityHBMConsumer();
+        return meta == 0 ? new TileEntityHBMConsumer() : new TileEntityHBMProducer();
     }
 }
