1b905bb5c472fbbf79bad2dd01d55e2a46cac9bd
Timo Ley <timo.ley@tilera.dev>
2024-06-25 20:22:44 +0200

    fix: adapt to new HBM energy system fixes #14 ref #15

--- src/main/java/net/anvilcraft/pccompat/tiles/TileEntityHBMConsumer.java
+++ src/main/java/net/anvilcraft/pccompat/tiles/TileEntityHBMConsumer.java
@@ -1,20 +1,21 @@
 package net.anvilcraft.pccompat.tiles;
 
-import api.hbm.energy.IEnergyConnector;
+import api.hbm.energymk2.IEnergyConnectorMK2;
+import api.hbm.energymk2.IEnergyReceiverMK2;
 import covers1624.powerconverters.tile.main.TileEntityEnergyConsumer;
 import net.anvilcraft.pccompat.mods.HBMProxy;
 import net.minecraft.util.MathHelper;
 import net.minecraftforge.common.util.ForgeDirection;
 
 public class TileEntityHBMConsumer
-    extends TileEntityEnergyConsumer<IEnergyConnector> implements IEnergyConnector {
+    extends TileEntityEnergyConsumer<IEnergyConnectorMK2> implements IEnergyReceiverMK2 {
     private boolean isLoaded = true;
     private boolean recursionBrake = false;
     private boolean transferLastTick = false;
     private double lastTransfer = 0.0;
 
     public TileEntityHBMConsumer() {
-        super(HBMProxy.powerSystem, 0, IEnergyConnector.class);
+        super(HBMProxy.powerSystem, 0, IEnergyConnectorMK2.class);
     }
 
     @Override
@@ -85,5 +86,10 @@ public class TileEntityHBMConsumer
         this.transferLastTick = true;
 
         return ret;
+    }
+
+    @Override
+    public void setPower(long power) {
+
     }
 }
--- src/main/java/net/anvilcraft/pccompat/tiles/TileEntityHBMProducer.java
+++ src/main/java/net/anvilcraft/pccompat/tiles/TileEntityHBMProducer.java
@@ -1,20 +1,20 @@
 package net.anvilcraft.pccompat.tiles;
 
-import api.hbm.energy.IEnergyConnector;
-import api.hbm.energy.IEnergyGenerator;
+import api.hbm.energymk2.IEnergyConnectorMK2;
+import api.hbm.energymk2.IEnergyProviderMK2;
 import covers1624.powerconverters.tile.main.TileEntityEnergyProducer;
 import net.anvilcraft.pccompat.mods.HBMProxy;
 import net.minecraft.util.MathHelper;
 import net.minecraftforge.common.util.ForgeDirection;
 
 public class TileEntityHBMProducer
-    extends TileEntityEnergyProducer<IEnergyConnector> implements IEnergyGenerator {
+    extends TileEntityEnergyProducer<IEnergyConnectorMK2> implements IEnergyProviderMK2 {
     private boolean isLoaded = true;
     private boolean recursionBrake = false;
     private long subBuffer = 0;
 
     public TileEntityHBMProducer() {
-        super(HBMProxy.powerSystem, 0, IEnergyConnector.class);
+        super(HBMProxy.powerSystem, 0, IEnergyConnectorMK2.class);
     }
 
     @Override
@@ -30,7 +30,7 @@ public class TileEntityHBMProducer
         this.subBuffer = toProduce;
 
         for (ForgeDirection dir : ForgeDirection.VALID_DIRECTIONS) {
-            this.sendPower(
+            this.tryProvide(
                 worldObj,
                 xCoord + dir.offsetX,
                 yCoord + dir.offsetY,
@@ -41,6 +41,7 @@ public class TileEntityHBMProducer
 
         this.recursionBrake = false;
 
+        //TODO: find a way for the energy transfer to work with cables
         long tmp = this.subBuffer;
         this.subBuffer = 0;
 
